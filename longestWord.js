029bd353a2b21a617bb21cbc03c50c0b635845c9
khalid-Ibne-Hussain <khalidtuhinbd@gmail.com>
2024-01-19 14:27:52 +0600

    reverse string, longest word

---
+++ longestWord.js
@@ -0,0 +1,16 @@
+function findLongestWord(sentence) {
+  const words = sentence.split(" ");
+  //   let longestWord = "";
+
+  console.log(words);
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }
+  }
+
+  return longestWord;
+}
+
+console.log(findLongestWord("The quick brown fox jumps over the lazy dog"));
---
+++ reverseString.js
@@ -0,0 +1,5 @@
+function reverseString(value) {
+  return value.split("").reverse().join("");
+}
+
+console.log(reverseString("Hello"));
